ecfb8a6c8fdfc88312e21584fbf022825edf7328
Arizval Mendoza <145447881+ariz31@users.noreply.github.com>
2026-07-25 02:07:33 +0800

    Load layered application stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { AppearanceProvider } from './design-system/theme';
 import { initializePwaUpdateControl } from './infrastructure/pwaUpdate';
-import './styles.css';
-import './phases456.css';
-import './phases789.css';
-import './phase10.css';
-import './ui-modernization.css';
+import './app.css';
 
 initializePwaUpdateControl();
 
